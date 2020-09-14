cbb478934671e30443052a568fe5c2ed05d79670
alex <jesus.galaz@softtek.com>
2020-09-14 17:16:19 -0500

    add form

--- client/src/components/Router/routes.js
+++ client/src/components/Router/routes.js
@@ -2,6 +2,7 @@ import Home from './../Home'
 import About from './../About'
 import Login from './../Login/Login'
 import Dashboard from './../Dashboard/Dashboard'
+import Users from './../User/Users'
 
 export const routes = [
   {
@@ -19,5 +20,9 @@ export const routes = [
   {
     path: '/about',
     component: About
+  },
+  {
+    path: '/users',
+    component: Users
   }
 ]
