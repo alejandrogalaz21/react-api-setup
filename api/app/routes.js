e256090aaba3357bbb3199a9b59ed34b91b52dc9
alex <jesus.galaz@softtek.com>
2020-09-14 16:36:40 -0500

    pull request

--- api/app/routes.js
+++ api/app/routes.js
@@ -1,5 +1,5 @@
-import { users } from './users/users.controller'
-
 import { auth } from './auth/auth.controller'
+import { users } from './users/users.controller'
+import { products } from './products/products.controller'
 
-export const apiRoutes = [auth, users]
+export const apiRoutes = [auth, users, products]
