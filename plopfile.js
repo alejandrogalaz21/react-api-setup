9716b794faad06bee98a86a72c86f7d883c750b0
alex <jesus.galaz@softtek.com>
2020-09-15 12:31:31 -0500

    remove not need it libs

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,4 @@
-module.exports = function (plop) {
+const config = plop => {
   // module generator
   plop.setGenerator('module', {
     description: 'application module logic',
@@ -24,52 +24,6 @@ module.exports = function (plop) {
       }
     ]
   })
-  // controller generator
-  plop.setGenerator('controller', {
-    description: 'application controller logic',
-    prompts: [
-      {
-        type: 'input',
-        name: 'dir',
-        message: 'what dir please ?'
-      },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'controller name please'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'api/app/{{dir}}/{{camelCase name}}.controller.js',
-        templateFile: 'plop/plop-templates/controller.hbs',
-        skipIfExists: true
-      }
-    ]
-  })
-  // model
-  plop.setGenerator('model', {
-    description: 'application controller logic',
-    prompts: [
-      {
-        type: 'input',
-        name: 'dir',
-        message: 'what dir please ?'
-      },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'model name please'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'api/app/{{dir}}/{{camelCase name}}.js',
-        templateFile: 'plop/plop-templates/model.hbs',
-        skipIfExists: true
-      }
-    ]
-  })
 }
+
+module.exports = config
