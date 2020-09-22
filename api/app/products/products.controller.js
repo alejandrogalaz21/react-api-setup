11fb884114e658bd2495cc1f327539d59ad26b5b
Jonathan Rodriguez <jonathan.rodriguezs@softtek.com>
2020-09-22 13:18:44 -0500

    feat: products controller with knex mysql connection

--- api/app/products/products.controller.js
+++ api/app/products/products.controller.js
@@ -1,8 +1,7 @@
 import { Router } from 'express'
 import { green, blue } from './../../helpers/chalk.helper'
 import { ErrorHandler } from '../../helpers/error.helper'
-import { knex } from '../../server/db/mysql.connection'
-
+import { knex } from '../../server/db/mysql/mysql.connection'
 
 export function productsController(product) {
   const router = new Router()
@@ -17,18 +16,6 @@ export function productsController(product) {
     try {
       blue('products > controller > create')
       const newEntry = req.body
-
-      //fields validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
-      // model validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
-      const result = await product.create(newEntry)
       const [id] = await knex('products').insert(newEntry)
       const [result] = await knex('products').where('id', '=', id)
       green(result)
@@ -42,7 +29,6 @@ export function productsController(product) {
     try {
       blue('products > controller > readMany')
       let query = req.query || {}
-      const result = await product.find(query).sort({ updatedAt: -1 })
       const result = await knex('products').select('*')
       green(result)
       return res.json(result)
@@ -66,17 +52,6 @@ export function productsController(product) {
   async function update(req, res, next) {
     try {
       blue('products > controller > update')
-
-      //fields validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
-      // model validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
       const changedEntry = req.body
       const { _id } = req.params
       await knex('products').where('id', '=', _id).update(changedEntry)
@@ -91,17 +66,6 @@ export function productsController(product) {
   async function remove(req, res, next) {
     try {
       blue('products > controller > remove')
-
-      //fields validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
-      // model validations
-      if (false) {
-        throw new ErrorHandler({ status: 400, message: 'validation message' })
-      }
-
       const { _id } = req.params
       const confirmation = await knex('products').where('id', '=', _id).del()
       const result = { id: _id, removed: new Boolean(confirmation) }
