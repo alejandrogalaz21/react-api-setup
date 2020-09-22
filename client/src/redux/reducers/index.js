924856378dbffeadb11669f2faa20e25c5361cb3
alex <jesus.galaz@softtek.com>
2020-09-22 15:08:18 -0500

    error in plop

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -5,7 +5,7 @@ import { createBrowserHistory } from 'history'
 import { connectRouter } from 'connected-react-router'
 // custom reducer's
 /* PLOP_INJECT_IMPORT */
-import { items } from './../../components/Item/item.redux'
+import { items } from './../../modules/Item/item.redux'
 import { products } from '../../modules/Product/product.redux'
 import { app } from './app.reducer'
 import { user } from './../../components/User/user.redux'
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,3 +1,3 @@
 /* PLOP_INJECT_EXPORT */
-export { itemsSagas } from './../../components/Item/item.saga'
+export { itemsSagas } from './../../modules/Item/item.saga'
 export { productsSagas } from '../../modules/Product/product.saga'
